201d181220536b92b4a23759668fe76c085d84d6
michal2630 <mike.mich1996@gmail.com>
2020-03-03 21:25:36 +0000

    Responsive footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,16 @@
 import React from "react";
-
+import { NavLink } from "react-router-dom";
+import { MdEmail } from "react-icons/md";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 const Footer = () => (
         <footer>
-            This is footer
-
+            <div className="footer-icon">
+                <a href="https://www.linkedin.com/in/micha%C5%82-michalski-772ab0151/"><FaLinkedin /></a>
+                <NavLink exact activeClassName="active" to="/contact">
+                    <MdEmail />
+                </NavLink>
+                <a href="https://github.com/michal2630"><FaGithub /></a>
+            </div>
         </footer>
 );
 
--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -1,8 +1,35 @@
 import React from "react";
+import {
+    DiBootstrap,
+    DiCss3,
+    DiReact,
+    DiGithubBadge,
+    DiJavascript1,
+    DiHtml5,
+    DiPhotoshop,
+    DiSass,
+    DiNpm,
+    DiGitBranch,
+    DiNodejs
+} from "react-icons/di";
+
 
 const Technologies = () => (
     <React.Fragment>
         <h1>Technologies I use</h1>
+            <div className="icons">
+                <DiBootstrap />
+                <DiGitBranch />
+                <DiNpm />
+                <DiSass />
+                <DiPhotoshop />
+                <DiHtml5 />
+                <DiJavascript1 />
+                <DiGithubBadge />
+                <DiReact />
+                <DiCss3 />
+                <DiNodejs />
+            </div>
     </React.Fragment>
 )
 
